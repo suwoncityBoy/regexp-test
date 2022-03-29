8186e7e03700a0a84c38be4f400cfa7d72dba8b6
suwoncityBoy <dlgkrrud00@naver.com>
2022-03-29 14:35:57 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const str = `
+010-1234-5678
+theasd@naver.com
+http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+hi THE hellow what are u doing.
+abbfcccdddd
+d`
+
+
+
+console.log(
+  str.match(/./g)
+  )
